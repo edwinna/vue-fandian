f4c43e890bff79f3dbd08323b45101dbc335af69
luoyuting5 <yutingl823@gmail.com>
2017-07-12 16:22:15 +0800

    using vue-cli init project

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style type="stylus" ref="stylesheet/stylus">
+.tab
+  display: flex
+  width:100%
+  height: 40px
+  line-height: 40px
+  .tab-item
+    flex: 1
+    text-align: center
+
+
+</style>
---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>This is rating page</div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style type="stylus" ref="stylesheet/stylus">
+
+</style>
